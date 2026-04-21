3aed1a5c3e539a9118f9dfa6ec7021261a2e7144
caden <muellercaden@gmail.com>
2026-04-21 12:14:26 -0500

    feat: remove public download path from site

--- site.js
+++ site.js
@@ -1,33 +1,12 @@
 const links = {
-  appRepo: "https://github.com/gc23-btc/directive-desktop",
   siteRepo: "https://github.com/gc23-btc/directive-site",
-  download: "https://github.com/gc23-btc/directive-desktop/releases/download/v0.1.0/Directive-0.1.0-arm64-mac.zip",
-  preview: "#",
 };
 
-const appRepoIds = ["app-repo-link", "app-repo-link-bottom"];
-const downloadIds = ["download-link", "download-link-bottom"];
+const siteRepoIds = ["site-repo-link", "site-repo-link-hero", "site-repo-link-bottom"];
 
-for (const id of appRepoIds) {
+for (const id of siteRepoIds) {
   const node = document.getElementById(id);
   if (node) {
-    node.href = links.appRepo;
+    node.href = links.siteRepo;
   }
-}
-
-for (const id of downloadIds) {
-  const node = document.getElementById(id);
-  if (node) {
-    node.href = links.download;
-  }
-}
-
-const siteRepoLink = document.getElementById("site-repo-link");
-if (siteRepoLink) {
-  siteRepoLink.href = links.siteRepo;
-}
-
-const sitePreviewLink = document.getElementById("site-preview-link");
-if (sitePreviewLink) {
-  sitePreviewLink.href = links.preview;
 }
